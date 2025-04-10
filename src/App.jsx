e4393acf23862d238c7fca2d8cb184776dc929e0
grahamt14 <grahamt14@gmail.com>
2025-04-10 12:46:49 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -34,12 +34,30 @@ function App() {
         <img src="src/assets/ListEasier.jpg" alt="ListEasier Image" class="logoCSS" />
       </div>
       <div className="card">
-	  <div class="file-upload">
-  <input onchange={handleClickUpload} type="file" name="fileToUpload" id="fileToUpload" />
-</div>
+	 <div class="file-upload">
+		<input type="file" name="fileToUpload" id="fileToUpload" />
+		// Be aware! We are handling only the first <input type="file" /> element
+// To avoid errors, it should be placed before this piece of code
+var input = document.querySelector('input[type=file]');
+
+// You will receive the Base64 value every time a user selects a file from his device
+// As an example I selected a one-pixel red dot GIF file from my computer
+input.onchange = function () {
+  var file = input.files[0],
+    reader = new FileReader();
+
+  reader.onloadend = function () {
+    // Since it contains the Data URI, we should remove the prefix and keep only Base64 string
+    var b64 = reader.result.replace(/^data:.+;base64,/, '');
+    console.log(b64); //-> "R0lGODdhAQABAPAAAP8AAAAAACwAAAAAAQABAAACAkQBADs="
+  };
+
+  reader.readAsDataURL(file);
+};
+	</div>
 <br></br>
         <button onClick={handleClickUpload}>
-          Assign to State
+          Upload File(s)
         </button>
 
         <button onClick={handleClick}>

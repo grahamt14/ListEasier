16860b79899f098ba0ca443850344d55c8f51909
grahamt14 <grahamt14@gmail.com>
2025-04-11 19:31:20 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import './App.css';
 function App() {
   const [responseData, setResponseData] = useState(null);
   const [filesBase64, setFilesBase64] = useState([]);
-  const [category, setCategory] = useState();
-  const [subCategory, setsubCategory] = useState();
 
   // Data representing the categories and their associated subcategories
   const data = {
@@ -30,7 +28,8 @@ function App() {
   };
 
   const handleSubCategoryChange = (e) => {
-    setsubCategory(e.target.value);
+    const subCategory = e.target.value;
+    setSubcategories(e.target.value);
   };
 
   const convertToBase64 = (file) => {

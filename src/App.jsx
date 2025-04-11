857da96c2533f7d5695d5017db40fa74cf3a62eb
grahamt14 <grahamt14@gmail.com>
2025-04-11 12:01:59 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -61,13 +61,7 @@ function App() {
       <input type="file" multiple accept="image/*" onChange={handleFileChange} />
       <div style={{ display: 'flex', gap: '1rem', flexWrap: 'wrap', marginTop: '1rem' }}>
         {filesBase64.map((src, index) => (
-          <img key={index} src={src} alt={`preview ${index}`} style={{ width: 100 }} />
-        ))}
-      </div>
-	  
-	  <div style={{ display: 'flex', gap: '1rem', flexWrap: 'wrap', marginTop: '1rem' }}>
-        {filesBase64.map((src, index) => (
-          <p>{src.split(',')[1]}</p>
+          <img key={index} src={src} alt={`preview ${index}`} style={{ width: 200 }} />
         ))}
       </div>
 	 
